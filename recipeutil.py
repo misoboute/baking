a0b4a5f22205876c5aec64647bd0dd7208a3ff67
Muhammad Ismail Soboute <misoboute@MacBook-Pro.local>
2020-10-05 00:36:42 +0200

    Add UnitConv for converting among units of mass and volume

--- recipeutil.py
+++ recipeutil.py
@@ -3,6 +3,64 @@ import json
 import os.path
 import string
 
+class UnitConv:
+    def __init__(self):
+        with open("Ingredient-Densities.json") as desitiesFile:
+            self._densities = json.load(desitiesFile)
+        with open("Unit-Conv-Tables.json") as convTableFile:
+            convTable = json.load(convTableFile)
+            self._massConvTab = convTable['mass']
+            self._volConvTab = convTable['volume']
+            self._massConvTab[''] = 1
+            self._volConvTab[''] = 1
+    
+    def get_density(self, material):
+        if not material:
+            raise RuntimeError(
+                'Material type must be specified when converting '
+                'mass to/from volum')
+
+        if not material in self._densities:
+            raise RuntimeError(
+                'Failed to determine density of material "{material}". '
+                'Entry not found!')
+
+        return self._densities[material]
+
+    def conv(self, value, toUnit, fromUnit = '', material = ''):
+        if (material and not material in self._densities):
+            raise RuntimeError('Unable to find density value for "{material}"')
+
+        for u in (toUnit, fromUnit):
+            for t in (self._massConvTab, self._volConvTab):
+                if (u in t): break
+            else:
+                raise RuntimeError(
+                    'Measurement unit "{u}" not found in conversion tables')
+
+        if (toUnit in self._massConvTab):
+            return self._conv_mass(value, toUnit, fromUnit, material)
+        elif (toUnit in self._volConvTab):
+            return self._conv_vol(value, toUnit, fromUnit, material)
+
+    def conv_fmt(self, value, toUnit, fromUnit = '', material = '', fmt = '{}'):
+        return (fmt + ' {}').format(
+            self.conv(value, toUnit, fromUnit, material), toUnit)
+
+    def _conv_mass(self, value, toUnit, fromUnit, material):
+        if (fromUnit in self._volConvTab):
+            valueG = (value * self.get_density(material) * 
+                self._volConvTab[fromUnit])
+            return valueG / self._massConvTab[toUnit]
+        return value * self._massConvTab[fromUnit] / self._massConvTab[toUnit]
+
+    def _conv_vol(self, value, toUnit, fromUnit, material):
+        if (fromUnit in self._massConvTab):
+            valueMl = (value / self.get_density(material) * 
+                self._massConvTab[fromUnit])
+            return valueMl / self._volConvTab[toUnit]
+        return value * self._volConvTab[fromUnit] / self._volConvTab[toUnit]
+
 class Adjuster:
     def __init__(self, templateFilePath, inputFilePath, outputFilePath):
         with open(templateFilePath) as templateFile:
